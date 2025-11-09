cf1631d36a8385a41e61c340227cf57b9c5fb429
DavidTruong1609 <DavidTruong1609@gmail.com>
2025-11-09 18:29:27 +1000

    refactor: restructure api and db insert functions

--- server/src/services/populateAnimeDatabase.ts
+++ server/src/services/populateAnimeDatabase.ts
@@ -4,50 +4,93 @@ import pool from "../config/db.ts";
 
 dotenv.config();
 
-interface AnimeNode {
+interface Anime {
     id: number;
     title: string;
     main_picture: object;
 }
 
-interface AnimeRanking {
-    ranking: number;
+interface AnimeDetails {
+    malId: number;
+    title: string;
+    source: string;
+    startSeason: string;
+    mean: number;
+    mediaType: string;
 }
-
-interface AnimeData {
-    node: AnimeNode;
-    ranking: AnimeRanking;
-}
-
-const url = "https://api.myanimelist.net/v2/anime/ranking";
 
 const headers = {
     "X-MAL-CLIENT-ID": process.env.MAL_CLIENT_ID,
 }
 
-const params = {
-    "ranking_type": "bypopularity", // Filtered by the top most popular anime on MyAnimeList.
-    "limit": 5, // Change this limit number variable to a max of 500 to add that amount of animes to your database.
+const fetchAnime = async (limit: number): Promise<Anime[]> => {
+    /**
+    Fetches the top most popular anime ids and titles from the MyAnimeList API.
+
+    @param {number} limit The number of anime to fetch from the API.
+    @returns {Promise<Anime[]>} Promise of the anime ids and titles.
+    */
+    const url = "https://api.myanimelist.net/v2/anime/ranking";
+    const params = {
+        "ranking_type": "bypopularity",
+        "limit": limit,
+    }
+
+    const response = await axios.get(url, { headers, params });
+
+    return response.data.data.map((data: { node: Anime; }) => data.node); 
 }
 
-const fetchAnimeData = async () => {
-    try {
-        const response = await axios.get(url, { headers, params });
+const fetchAnimeDetails = async (animeId: number): Promise<AnimeDetails | undefined> => {
+    /**
+    Fetches the anime details from their anime id from the MyAnimeList API.
 
-        return response.data.data;
+    @param {number} animeId The MyAnimeList anime id to fetch.
+    @returns {Promise<AnimeDetails | undefined>} Promise of the anime details or undefined if fails.
+    */
+    const url = "https://api.myanimelist.net/v2/anime";
+    const params = {
+        "fields": "id, title, alternative_titles, mean, media_type, genres, start_season, source, studios"
+    }
+
+    try {
+        const response = await axios.get(`${url}/${animeId}`, { headers, params });
+
+        return {
+            title: response.data.title,
+            source: response.data.source, 
+            startSeason: `${response.data.start_season.season} ${response.data.start_season.year}`,
+            mean: response.data.mean,
+            mediaType: response.data.media_type,
+            malId: response.data.id
+        };
     }
     catch (error) {
         console.error('There was an error with the fetch operation:', error);
-        return [];
     }
 }
 
-const insertAnimeData = async (animeData: AnimeData[]): Promise<void> => {
-    try {
-        for (const anime of animeData) {
-            const {id, title} = anime.node
+const insertAnime = async (animeDetails: AnimeDetails[]): Promise<void> => {
+    /**
+    Inserts the array of anime details into the Postgresql database.
 
-            await pool.query("INSERT INTO anime (mal_id, title) VALUES ($1, $2)", [id, title])
+    @param {AnimeDetails[]} animeDetails The array of anime details.
+    */
+    try {
+        for (const anime of animeDetails) {
+            const {malId, title, source, startSeason, mean, mediaType} = anime
+
+            await pool.query(
+                `INSERT INTO anime (mal_id, title, source, start_season, mean, media_type) 
+                VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT (mal_id) 
+                DO UPDATE SET 
+                title = EXCLUDED.title, 
+                source = EXCLUDED.source, 
+                start_season = EXCLUDED.start_season, 
+                mean = EXCLUDED.mean, 
+                media_type = EXCLUDED.media_type`, 
+                [malId, title, source, startSeason, mean, mediaType]
+            )
         }
         console.log('Anime data successfully inserted into the database');
     }
@@ -57,16 +100,25 @@ const insertAnimeData = async (animeData: AnimeData[]): Promise<void> => {
 }
 
 const populateDatabase = async () => {
-    try {
-        const animeData = await fetchAnimeData(); 
-        console.log(animeData); 
-        console.log(animeData.length)
+    /**
+    Populates the database with anime data.
 
-        await insertAnimeData(animeData);
-    } 
-    catch (error) {
-        console.error("Error fetching anime data:", error);
+    */
+    const animeLimit = 7 // Change this limit number variable to a max of 500 to add that amount of animes to your database.
+
+    const animeData = await fetchAnime(animeLimit); 
+
+    const allAnimeData = []
+
+    for (const anime of animeData) {
+        const animeDetails = await fetchAnimeDetails(anime.id)
+
+        if (animeDetails) {
+            allAnimeData.push(animeDetails)
+        }
     }
+
+    await insertAnime(allAnimeData);
 };
 
 populateDatabase();
