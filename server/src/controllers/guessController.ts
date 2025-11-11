5ed031315e3e7ce9efbc613ebdb1f51f6aafa48a
DavidTruong1609 <DavidTruong1609@gmail.com>
2025-11-11 23:26:48 +1000

    feat: update guess query function

--- server/src/controllers/guessController.ts
+++ server/src/controllers/guessController.ts
@@ -1,36 +1,52 @@
-import type { Request, Response } from 'express';
+import type { Request, Response } from "express";
 import pool from "../config/db.ts";
 
 export const getGuesses = async (req: Request, res: Response) => {
     try {
         const allGuesses = await pool.query(
-            `SELECT guess.id as guess_id, guess.anime_id, anime.title, anime.source, anime.start_season, anime.mean
-            FROM guesses guess 
-            INNER JOIN anime anime ON guess.anime_id = anime.mal_id
-            ORDER BY guess.created_at DESC;`
+            `SELECT guesses.anime_id, anime.title, anime.source, anime.start_season, anime.mean, anime.media_type,
+            jsonb_agg(DISTINCT genres.name) AS genres,
+            jsonb_agg(DISTINCT studios.name) AS studios
+            FROM guesses
+			LEFT JOIN anime ON anime.mal_id = guesses.anime_id
+            LEFT JOIN anime_genres ON anime_genres.anime_id = anime.mal_id
+            LEFT JOIN genres ON genres.id = anime_genres.genre_id
+            LEFT JOIN anime_studios ON anime_studios.anime_id = anime.mal_id
+            LEFT JOIN studios ON studios.id = anime_studios.studio_id
+            GROUP BY guesses.id, anime.title, anime.source, anime.start_season, anime.mean, anime.media_type`
         )
-        res.json(allGuesses.rows)
+        const filteredGuesses = allGuesses.rows.map((guess) => ({
+            animeId: guess.anime_id,
+            title: guess.title,
+            source: guess.source,
+            startSeason: guess.start_season,
+            mean: guess.mean,
+            mediaType: guess.media_type,
+            genres: guess.genres,
+            studios: guess.studios,
+        }))
+        res.status(200).json(filteredGuesses)
     }
     catch (error) {
         if (error instanceof Error) {
-            console.log(error.message)
-            res.status(500).json({ message: 'Error fetching all guesses' });
+            console.error("Error querying all guesses: ", error.message)
+            res.status(500).send("Error querying all guesses.")
         }
     }
 }
 
 export const postGuess = async (req: Request, res: Response) => {
     try {
-        const { animeId, correct } = req.body;
+        const { animeId, correct } = req.body
 
-        const postGuess = await pool.query("INSERT INTO guesses (anime_id, correct) VALUES ($1, $2) ON CONFLICT DO NOTHING", [animeId, correct || false])
+        await pool.query("INSERT INTO guesses (anime_id, correct) VALUES ($1, $2) ON CONFLICT DO NOTHING", [animeId, correct || false])
 
-        res.json(postGuess.rows[0]);
+        res.status(200).json({ message: "Successfully posted guess." })
     }
     catch (error) {
         if (error instanceof Error) {
-            console.log(error.message)
-            res.status(500).json({ message: 'Error posting guess' });
+            console.error("Error posting guess into database: ", error.message)
+            res.status(500).send("Error posting guess into database.")
         }
     }
 }
@@ -39,12 +55,12 @@ export const deleteGuesses = async (req: Request, res: Response) => {
     try {
         await pool.query("DELETE FROM guesses")
         
-        res.status(200).json({ message: 'All guesses deleted successfully.' });
+        res.status(200).json({ message: "Successfully deleted all guesses." })
     }
     catch (error) {
         if (error instanceof Error) {
-            console.log(error.message)
-            res.status(500).json({ message: 'Error deleting guess' });
+            console.log("Error deleting guesses: ", error.message)
+            res.status(500).send("Error deleting guesses.")
         }
     }
 }
