19e6a86cd92519fb3036fad7461092a70af2cae2
DavidTruong1609 <DavidTruong1609@gmail.com>
2025-11-11 23:14:56 +1000

    refactor: remove anime genre and studio routes.

--- server/src/routes/animeRoutes.ts
+++ server/src/routes/animeRoutes.ts
@@ -1,13 +1,10 @@
 import { Router } from "express";
-import { getAnimeDetails, getAnimeGenres, getAnimeStudios, getAnimeTitles, getRandomAnime } from "../controllers/animeController.ts";
+import { getAnimeDetails, getAnimeTitles, getRandomAnime } from "../controllers/animeController.ts";
 
 const router = Router();
 
 router.get("/anime", getAnimeDetails)
 router.get("/anime-titles", getAnimeTitles)
-router.get("/anime-genres", getAnimeGenres)
-router.get("/anime-studios", getAnimeStudios)
 router.get("/random-anime", getRandomAnime)
-
 
 export default router;
