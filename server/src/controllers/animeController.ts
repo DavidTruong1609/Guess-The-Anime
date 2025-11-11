414b3db1cc2d958a02c9d41566d0200bbba575e0
DavidTruong1609 <DavidTruong1609@gmail.com>
2025-11-11 23:22:30 +1000

    feat: add random anime query function and update get all anime query function

--- server/src/controllers/animeController.ts
+++ server/src/controllers/animeController.ts
@@ -1,67 +1,93 @@
-import type { Request, Response } from 'express';
+import type { Request, Response } from "express"
 import pool from "../config/db.ts"
 
 export const getAnimeDetails = async (req: Request, res: Response) => {
     try {
-        const allAnime = await pool.query("SELECT * from anime")
-        res.status(200).json(allAnime.rows)
+        const allAnimeDetails = await pool.query(
+            `SELECT mal_id, title, source, start_season, mean, media_type,
+            jsonb_agg(DISTINCT genres.name) as genres,
+            jsonb_agg(DISTINCT studios.name) as studios
+            FROM anime
+            LEFT JOIN anime_genres ON anime_genres.anime_id = anime.mal_id
+            LEFT JOIN genres ON genres.id = anime_genres.genre_id
+            LEFT JOIN anime_studios ON anime_studios.anime_id = anime.mal_id
+            LEFT JOIN studios ON studios.id = anime_studios.studio_id
+            GROUP BY anime.id`
+        )
+
+        const formattedAnime = allAnimeDetails.rows.map((anime) => ({
+            malId: anime.mal_id,
+            title: anime.title,
+            source: anime.source,
+            startSeason: anime.start_season,
+            mean: anime.mean,
+            mediaType: anime.media_type,
+            genres: anime.genres,
+            studios: anime.studios,
+        }))
+
+        res.status(200).json(formattedAnime)
     }
     catch (error) {
         if (error instanceof Error) {
-            console.log(error.message)
-            res.status(500).json({ message: 'Error fetching anime list' });
+            console.log("Error querying anime list: ", error.message)
+            res.status(500).send("Error querying anime list.")
         }
     }
 }
 
 export const getAnimeTitles = async (req: Request, res: Response) => {
     try {
-        const allAnimeTitles = await pool.query("SELECT * from anime_titles")
-        res.status(200).json(allAnimeTitles.rows)
-    }
-    catch (error) {
-        if (error instanceof Error) {
-            console.log(error.message)
-            res.status(500).send("Server Error")
-        }
-    }
-}
+        const allAnimeTitles = await pool.query("SELECT anime_id, title FROM anime_titles")
 
-export const getAnimeGenres = async (req: Request, res: Response) => {
-    try {
-        const allAnimeGenres = await pool.query("SELECT * from anime_genres")
-        res.status(200).json(allAnimeGenres.rows)
+        const formatttedAnimeTitles = allAnimeTitles.rows.map((anime) => ({
+            animeId: anime.anime_id,
+            title: anime.title,
+        }))
+        
+        res.status(200).json(formatttedAnimeTitles)
     }
     catch (error) {
         if (error instanceof Error) {
-            console.log(error.message)
-            res.status(500).send("Server Error")
-        }
-    }
-}
-
-export const getAnimeStudios = async (req: Request, res: Response) => {
-    try {
-        const allAnimeStudios = await pool.query("SELECT * from anime_studios")
-        res.status(200).json(allAnimeStudios.rows)
-    }
-    catch (error) {
-        if (error instanceof Error) {
-            console.log(error.message)
-            res.status(500).send("Server Error")
+            console.error("Error querying anime titles: ", error.message)
+            res.status(500).send("Error querying anime titles.")
         }
     }
 }
 
 export const getRandomAnime = async (req: Request, res: Response) => {
     try {
-        const randomAnime = await pool.query("SELECT * FROM anime ORDER BY RANDOM() LIMIT 1")
-        res.status(200).json(randomAnime.rows[0]);
+        const randomAnime = await pool.query(
+            `SELECT mal_id, title, source, start_season, mean, media_type,
+            jsonb_agg(DISTINCT genres.name) as genres,
+            jsonb_agg(DISTINCT studios.name) as studios
+            FROM anime
+            LEFT JOIN anime_genres ON anime_genres.anime_id = anime.mal_id
+            LEFT JOIN genres ON genres.id = anime_genres.genre_id
+            LEFT JOIN anime_studios ON anime_studios.anime_id = anime.mal_id
+            LEFT JOIN studios ON studios.id = anime_studios.studio_id
+            GROUP BY anime.id
+            ORDER BY RANDOM()
+            LIMIT 1`
+        )
+
+        const formattedRandomAnime = randomAnime.rows.map((anime) => ({
+            malId: anime.mal_id,
+            title: anime.title,
+            source: anime.source,
+            startSeason: anime.start_season,
+            mean: anime.mean,
+            mediaType: anime.media_type,
+            genres: anime.genres,
+            studios: anime.studios,
+        }))
+
+        res.status(200).json(formattedRandomAnime[0])
     }
     catch (error) {
         if (error instanceof Error) {
-            console.error("Error fetching random anime:", error);
-            res.status(500).json({ error: "Failed to get random anime." });
+            console.error("Error querying random anime: ", error.message)
+            res.status(500).send("Error querying random anime.")
         }
     }
 }
