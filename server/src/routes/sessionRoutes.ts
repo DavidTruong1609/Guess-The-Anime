f7e1f91b2f976f2e3a3d88e2fd053f025d116965
DavidTruong1609 <DavidTruong1609@gmail.com>
2025-11-12 16:38:11 +1000

    feat: update session routes

--- server/src/routes/sessionRoutes.ts
+++ server/src/routes/sessionRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { postSession } from "../controllers/sessionController.ts";
+import { deleteSession, getSession, postSession } from "../controllers/sessionController.ts";
 
 const router = Router();
 
-router.post("/get-guesses", postSession)
+router.get("/get-session", getSession)
+router.post("/post-session", postSession)
+router.delete("/delete-session", deleteSession)
 
 export default router;
