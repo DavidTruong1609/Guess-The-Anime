35694fd62a952d4733a1b307e0219a961a1908b3
DavidTruong1609 <DavidTruong1609@gmail.com>
2025-11-12 16:38:43 +1000

    feat:  add get and delete session query functions

--- server/src/controllers/sessionController.ts
+++ server/src/controllers/sessionController.ts
@@ -1,11 +1,29 @@
 import type { Request, Response } from "express"
 import pool from "../config/db.ts"
 
+export const getSession = async (req: Request, res: Response) => {
+    try {
+        const gameSession = await pool.query("SELECT anime_id FROM game_session")
+
+        const formattedGameSession = gameSession.rows.map((session) => ({
+            animeId: session.anime_id,
+        }))
+
+        res.status(200).json(formattedGameSession[0])
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            console.error("Error posting game session into database: ", error.message)
+            res.status(500).send("Error posting game session into database.")
+        }
+    }
+}
+
 export const postSession = async (req: Request, res: Response) => {
     try {
         const { animeId } = req.body
 
-        await pool.query("INSERT INTO game_session (anime_id) VALUES ($1) ON CONFLICT DO NOTHING", [animeId])
+        await pool.query("INSERT INTO game_session (anime_id) VALUES ($1) ON CONFLICT DO NOTHING RETURNING *", [animeId])
 
         res.status(200).json({ message: "Successfully posted game session." })
     }
@@ -13,6 +31,20 @@ export const postSession = async (req: Request, res: Response) => {
         if (error instanceof Error) {
             console.error("Error posting game session into database: ", error.message)
             res.status(500).send("Error posting game session into database.")
+        }
+    }
+}
+
+export const deleteSession = async (req: Request, res: Response) => {
+    try {
+        await pool.query("DELETE FROM game_session")
+
+        res.status(200).json({ message: "Successfully deleted game session." })
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            console.error("Error deleting game session from database: ", error.message)
+            res.status(500).send("Error deleting game session from database.")
         }
     }
 }
