203e964891f9af606bf424687b1af033ab8615c8
DavidTruong1609 <DavidTruong1609@gmail.com>
2025-11-12 12:35:40 +1000

    feat: add session routes to index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import helmet from "helmet";
 import animeRoutes from "./routes/animeRoutes.ts"
 import guessRoutes from "./routes/guessRoutes.ts";
+import sessionRoutes from "./routes/sessionRoutes.ts";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.use("/", animeRoutes);
 app.use("/guess", guessRoutes)
+app.use("/session", sessionRoutes)
 
 app.listen(port, () => {
     console.log(`Server running on localhost http://localhost:${port}/`)
