abb9e6558f666d2db4c4c587d75544ee28d2bfe5
DavidTruong1609 <DavidTruong1609@gmail.com>
2025-11-17 13:47:48 +1000

    feat: update styling for input and suggestions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 
 import axios from "axios"
 import { ImArrowDown, ImArrowUp } from "react-icons/im";
+import { MdArrowDownward, MdArrowUpward } from "react-icons/md";
 
 function App() {
   interface AnimeTitle {
@@ -128,7 +129,7 @@ function App() {
     } 
     else {
       const filtered = animeTitles.filter((anime) => anime.title.toLowerCase().includes(searchValue.toLowerCase()));
-      setAnimeSearchTitles(filtered.slice(0, 10));
+      setAnimeSearchTitles(filtered.slice(0, 15));
     }
   };
 
@@ -180,71 +181,82 @@ function App() {
     <>
       <div className="bg-cover min-h-screen bg-neutral-800 bg-fixed">
 
-        <div className="flex justify-end p-2">
+        <div className="flex justify-end p-6">
           <button 
             onClick={handleNewGame}
-            className="rounded-3xl bg-neutral-500 px-4 py-2 font-bold text-white cursor-pointer hover:bg-neutral-300 hover:text-neutral-700"
+            className="rounded-4xl bg-linear-to-r from-violet-500 to-blue-500 px-6 py-3 font-bold text-neutral-100 cursor-pointer hover:text-neutral-800"
           >New Game</button>
         </div>
 
-        <div className="flex justify-center items-center py-6">
 
-          <div className="w-1/3 bg-neutral-300 rounded-2xl text-center py-6 opacity-95 border-6 border-neutral-500">
 
-            <div className="text-white font-bold text-6xl py-6 px-2">
-              <h1>Guess The Anime</h1>
-            </div>
+        <div className="font-[Inter] text-neutral-100 font-semibold text-5xl text-center my-6">
+          <h1>Guess The Anime</h1>
+        </div>
 
-            <div className="py-6 px-2">
+        <div className="flex justify-center items-center pb-[30px]">
 
-              <div className="">
-                <form onSubmit={onSubmit}>
+          <div className="w-1/2">
+
+            <form 
+              onSubmit={onSubmit}
+              className="flex justify-center">
+
+              <div className="relative w-1/2 mx-6">
+                <div className="p-0.5 bg-linear-to-r from-violet-500 to-blue-500 rounded-4xl">
                   <input
                     type="text"
                     value={search}
                     onChange={handleSearchChange}
                     placeholder="Guess anime..."
                     disabled={gameEnded}
-                    className="bg-white px-4 py-2 w-1/2 rounded-4xl mx-2 font-bold"
+                    className="bg-neutral-100 px-6 py-3 w-full rounded-4xl font-[Inter] focus:outline-none"
                   />
-                  <button
-                    type="submit"
-                    disabled={gameEnded}
-                    className="bg-linear-to-r from-violet-500 to-blue-500 px-6 py-2 rounded-4xl text-lg mx-2 text-white font-bold cursor-pointer hover:bg-neutral-200 hover:text-neutral-800"
-                  >Guess</button>
-                </form>
-              </div>
-              
-              <div>
-                {animeSearchTitles.length === 0 ? (
-                  <div></div>
-                ) : (
-                  <div className="bg-neutral-500">
-                    {animeSearchTitles.map((animeTitle) => (
-                      <div
-                        key={animeTitle.title}
-                        onClick={() => handleSuggestionClick(animeTitle)}
-                      >
-                        <h5>{animeTitle.title}</h5>
-                      </div>
-                    ))}
-                  </div>
-                )}
+                </div>
+                <div className="w-full absolute ">
+                  {animeSearchTitles.length === 0 ? (
+                    <div></div>
+                  ) : (
+                    <div className="bg-neutral-100 rounded-b-lg font-[Inter] text-neutral-800 text-center">
+                      {animeSearchTitles.map((animeTitle) => (
+                        <div
+                          key={animeTitle.title}
+                          onClick={() => handleSuggestionClick(animeTitle)}
+                          className="hover:bg-neutral-300 px-2 py-1 transition"
+                        >
+                          <h5>{animeTitle.title}</h5>
+                        </div>
+                      ))}
+                    </div>
+                  )}
+                </div>
               </div>
 
-            </div>
+              <button
+                type="submit"
+                disabled={gameEnded}
+                className="bg-linear-to-r from-violet-500 to-blue-500 px-6 py-3 rounded-4xl text-neutral-100 font-bold cursor-pointer hover:text-neutral-800"
+              >Guess</button>
+
+            </form>
 
           </div>
 
         </div>
 
-        <div className="flex justify-center py-6">
+
+
+        <div className="flex justify-center my-[30px]">
+          <hr className="w-[1392px] rounded-4xl p-px bg-linear-to-r from-violet-500 to-blue-500"></hr>
+        </div>
+
+        <div className="flex justify-center pb-6 pt-[30px]">
           {animeGuesses.length === 0 ? (
             <div></div>
           ) : (
             <div className="w-[1392px]">
 
-              <div className="font-[Inter] grid grid-cols-8 gap-6 pb-3">
+              <div className="font-[Inter] grid grid-cols-8 gap-6 pb-3 text-neutral-800">
                 <div className="text-base py-3 px-8 bg-neutral-100 text-center rounded-4xl col-span-2">Title</div>
                 <div className="text-base py-3 px-8 bg-neutral-100 text-center rounded-4xl">Premiered</div>
                 <div className="text-base py-3 px-8 bg-neutral-100 text-center rounded-4xl">Genre/s</div>
@@ -257,40 +269,43 @@ function App() {
               <div className="">
                 {animeGuesses.map((animeGuess) => {
                   return (
-                    <div className="mt-3 p-0.5 bg-linear-to-r from-violet-500 to-blue-500 rounded-xl">
+                    <div
+                      key={animeGuess.animeId}
+                      className="mt-3 p-0.5 bg-linear-to-r from-violet-500 to-blue-500 rounded-xl">
                       <div
                         key={animeGuess.animeId}
-                        className={`min-h-24 rounded-xl grid grid-cols-8 p-3 bg-neutral-700`}>
-                        <div className={`font-bold flex items-center text-lg col-span-2 ${animeGuess.correct ? "text-emerald-400" : "text-red-400"}`}
+                        className="font-[Inter] text-base rounded-xl grid grid-cols-8 min-h-32 bg-neutral-700 hover:bg-neutral-600">
+                        <div className={`font-bold flex items-center col-span-2 ${animeGuess.correct ? "text-emerald-400 font-bold" : "text-red-400"}`}
                         >
                           <img src="https://cdn.myanimelist.net/images/anime/10/78745l.webp" className="h-24 px-4"></img>
                           {animeGuess.title}
                           </div>
-                        <div className="relative flex items-center">
-                          <div className="absolute inset-0 flex justify-center items-center z-10 text-lg">
+                        <div className="flex justify-center items-center">
+                          <div className="flex justify-center items-center">
                             <div
-                              className={`${randomAnime?.startSeason.includes(animeGuess.startSeason.split(" ")[0]) ? "text-emerald-400" : "text-red-400"}`}
+                              className={randomAnime?.startSeason.includes(animeGuess.startSeason.split(" ")[0]) ? "text-emerald-400 font-bold" : "text-red-400"}
                             >{animeGuess.startSeason.split(" ")[0].charAt(0).toUpperCase() + animeGuess.startSeason.split(" ")[0].slice(1)}</div>
                             <span>&nbsp;</span>
                             <div
-                              className={`${randomAnime?.startSeason.includes(animeGuess.startSeason.split(" ")[1]) ? "text-emerald-400" : "text-red-400"}`}
+                              className={randomAnime?.startSeason.includes(animeGuess.startSeason.split(" ")[1]) ? "text-emerald-400 font-bold" : "text-red-400"}
                             >{animeGuess.startSeason.split(" ")[1]}</div>
                           </div>
-                          <div className="absolute inset-0 flex justify-center items-center z-0 text-6xl opacity-25 text-red-400">
+                          <span>&nbsp;</span>
+                          <div className="text-2xl text-red-400">
                             {randomAnime?.startSeason !== undefined && (
                               parseInt(animeGuess.startSeason.split(" ")[1]) > parseInt(randomAnime.startSeason.split(" ")[1]) ? (
-                                <ImArrowDown/>
+                                <MdArrowDownward/>
                               ) : parseInt(animeGuess.startSeason.split(" ")[1]) < parseInt(randomAnime.startSeason.split(" ")[1]) ? (
-                                <ImArrowUp/>
+                                <MdArrowUpward/>
                               ) : null)}
                           </div>
                         </div>
-                        <div className="flex justify-center items-center text-center">
+                        <div className="flex justify-center items-center text-center py-3">
                           <div>
                             {animeGuess.genres.map((genre) => (
                               <div
                                 key={genre}
-                                className={randomAnime?.genres.includes(genre) ? "text-emerald-400" : "text-red-400"}
+                                className={randomAnime?.genres.includes(genre) ? "text-emerald-400 font-bold" : "text-red-400"}
                               >{genre}</div>
                             ))}
                           </div>
@@ -300,25 +315,26 @@ function App() {
                               {animeGuess.studios.map((studio) => (
                                 <div
                                   key={studio}
-                                  className={randomAnime?.studios.includes(studio) ? "text-emerald-400" : "text-red-400"}
+                                  className={randomAnime?.studios.includes(studio) ? "text-emerald-400 font-bold" : "text-red-400"}
                                 >{studio}</div>
                               ))}
                             </div>
                         </div>
                         <div
-                          className={`flex justify-center items-center ${animeGuess.source == randomAnime?.source ? "text-emerald-400" : "text-red-400"}`}
+                          className={`flex justify-center items-center ${animeGuess.source == randomAnime?.source ? "text-emerald-400 font-bold" : "text-red-400"}`}
                         >{animeGuess.source.charAt(0).toUpperCase() + animeGuess.source.slice(1).replace(/_/g, " ")}</div>
                         <div
-                          className={`flex justify-center items-center ${animeGuess.mediaType == randomAnime?.mediaType ? "text-emerald-400" : "text-red-400"}`}
+                          className={`flex justify-center items-center ${animeGuess.mediaType == randomAnime?.mediaType ? "text-emerald-400 font-bold" : "text-red-400"}`}
                         >{animeGuess.mediaType}</div>
-                        <div className={`relative ${animeGuess.mean == randomAnime?.mean ? "text-emerald-400" : "text-red-400"}`}>
-                          <div className="absolute inset-0 flex justify-center items-center z-10 text-lg">{animeGuess.mean}</div>
-                          <div className="absolute inset-0 flex justify-center items-center z-0 text-6xl opacity-25">
+                        <div className={`flex justify-center items-center ${animeGuess.mean == randomAnime?.mean ? "text-emerald-400 font-bold" : "text-red-400"}`}>
+                          <div>{animeGuess.mean}</div>
+                          <span>&nbsp;</span>
+                          <div className="text-2xl">
                             {randomAnime?.mean !== undefined && (
                               animeGuess.mean > randomAnime.mean ? (
-                                <ImArrowDown/>
+                                <MdArrowDownward/>
                               ) : animeGuess.mean < randomAnime.mean ? (
-                                <ImArrowUp/>
+                                <MdArrowUpward/>
                               ) : null)}
                           </div>
                         </div>
